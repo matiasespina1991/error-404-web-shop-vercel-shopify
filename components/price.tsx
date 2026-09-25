8235c13dad2cc2d39b87924ca7bbb0eaddf0a171
Matias Espina <matiasespina1991@gmail.com>
2026-09-26 00:44:21 +0200

    .

--- components/price.tsx
+++ components/price.tsx
@@ -1,25 +1,18 @@
-import clsx from "clsx";
-
 const Price = ({
   amount,
   className,
   currencyCode = "EUR",
-  currencyCodeClassName,
 }: {
   amount: string;
   className?: string;
-  currencyCode: string;
-  currencyCodeClassName?: string;
+  currencyCode?: string;
 } & React.ComponentProps<"p">) => (
-  <p suppressHydrationWarning={true} className={className}>
-    {`${new Intl.NumberFormat(undefined, {
-      style: "currency",
-      currency: currencyCode,
-      currencyDisplay: "narrowSymbol",
-    }).format(parseFloat(amount))}`}
-    <span
-      className={clsx("ml-1 inline", currencyCodeClassName)}
-    >{`${currencyCode}`}</span>
+  <p suppressHydrationWarning className={className}>
+    {new Intl.NumberFormat(undefined, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(parseFloat(amount))}{" "}
+    {currencyCode === "EUR" ? "€" : currencyCode}
   </p>
 );
 
